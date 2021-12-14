105aa1518ed7e90509f631a26e92302c0e53db37
anjali2164 <valisetianjali216@gmail.com>
2021-12-14 10:31:09 +0530

    javaassignment2

---
+++ java assignment/year.java
@@ -0,0 +1,19 @@
+public class year {
+
+    public static void main(String[] args){
+
+        int i;
+
+            int days[] = {31, 28, 31, 30, 31,30,31,31,30,31,31};
+          
+
+            String months[] = {"January", "febuary", "march", "april", "may", "june", "july", "august", "september", "november", "december"};
+           
+            for(i=0; i<=12; i++){
+
+           System.out.println(months[i] + " " + "has" +" " + days[i] +" " + "days");
+
+                                }
+    }
+    
+}
